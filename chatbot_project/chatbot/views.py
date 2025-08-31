6a5187c98ab24df4c926518af257c5f64b199c41
business-AK <160372324+business-AK@users.noreply.github.com>
2025-08-31 17:47:20 +0530

    Update views.py

--- chatbot_project/chatbot/views.py
+++ chatbot_project/chatbot/views.py
@@ -6,8 +6,8 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from .api import make_query, process_query
 
-API_KEY_CUSTOM_SEARCH = "AIzaSyDSGy9kiTdx_FaXS6KoKfce_Q9SxSQloLI"
-API_KEY_VISION = "AIzaSyBT-kDQY5tMoPAsgvJI9ze40NYIB4fko3U"
+API_KEY_CUSTOM_SEARCH = ""
+API_KEY_VISION = ""
 
 def fetch_news(query):
     """Fetches news using the Custom Search API"""
